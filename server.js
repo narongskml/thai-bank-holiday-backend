ac23bcdc4f3289645a77983066bf691039e80448
Narong Sungkhamalai <narong.skml@gmail.com>
2025-05-15 01:46:11 +0700

    change storage to vercel blob

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ const express = require("express");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
+const { list,put } = require("@vercel/blob");
+const { json } = require("stream/consumers");
+
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-
+const BLOB_STORAGE_URL= process.env.BLOB_STORAGE_URL
+const BLOB_READ_WRITE_TOKEN = process.env.BLOB_READ_WRITE_TOKEN;
 app.use(express.static(path.join(__dirname, 'public')));
+const BLOB_FOLDER = "bankholiday";
 
 const DATA_FOLDER = path.join(__dirname, "data");
 app.use(express.static("public")); // Serve frontend
@@ -52,27 +56,45 @@ function authMiddleware(req, res, next) {
   .map(origin => origin.trim())
   .filter(Boolean);
 
-  console.log(allowedOrigins);
+  console.log(`allowOrigins: ${allowedOrigins}`);
   const isAllowed = allowedOrigins.some(origin => referer.startsWith(origin));
-  console.log(isAllowed);
+  console.log(`isAllowed: ${isAllowed}`);
+  /*
   if (!isAllowed) {
     return res.status(403).json({ error: "Access denied from this origin" });
   }
+    */
   next();
 }
 
 
 // API: Get cached holidays
-app.get("/api/holidays/:year", authMiddleware, (req, res) => {
+app.get("/api/holidays/:year", authMiddleware, async (req, res) => {
   const year = req.params.year;
-  const filePath = path.join(DATA_FOLDER, `${year}.json`);
-  console.log(req);
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).json({ error: "Data not available" });
+  console.log(`year: ${ year }`);
+  const response = await list();
+  if (!response) {
+      return res.status(404).json({ error: 'data not found' });
+    }
+
+  if (response) {
+    const data = await response.blobs.filter((meta) => meta.size>0); // or blob.json() for JSON parsing
+    console.log(data);
+    data.forEach(content => {
+      if (content.pathname.includes(year))
+      {
+         console.log('url:'+content.url);
+         fetch(content.url)
+        .then((f) => f.json())
+        .then((f) => res.json(f))
+        .catch((err) => console.log(err));
+       
+       
+      }
+    });
+    
   }
 
-  const data = fs.readFileSync(filePath);
-  res.type("json").send(data);
 });
 
 // Admin route: Fetch from BOT and store
@@ -82,21 +104,19 @@ app.get("/admin/fetch/:year", async (req, res) => {
   const url = `https://apigw1.bot.or.th/bot/public/financial-institutions-holidays/?year=${year}`;
 
   try {
-    const response = await axios.get(url, {
-      headers: {
-        "X-IBM-Client-Id": process.env.BOT_API_KEY,
-        "accept": "application/json",
-      },
-    });
-
-    if (!fs.existsSync(DATA_FOLDER)) {
-      fs.mkdirSync(DATA_FOLDER);
-    }
-
-    const filePath = path.join(DATA_FOLDER, `${year}.json`);
-    fs.writeFileSync(filePath, JSON.stringify(response.data, null, 2));
-
-    res.json({ status: "saved", file: `${year}.json` });
+      const response = await axios.get(url, {
+        headers: {
+          "X-IBM-Client-Id": process.env.BOT_API_KEY,
+          "accept": "application/json",
+        },
+      });
+     /** write to vercel blob */
+     const blob = await put(`${BLOB_FOLDER}/${year}.json`, JSON.stringify(response.data, null, 2) , {
+        contentType: 'application/json',
+        access: 'public',
+        allowOverwrite: true,
+      });
+      res.json({ status: "saved", file: `${year}.json` });
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ error: "Failed to fetch from BOT API" });
